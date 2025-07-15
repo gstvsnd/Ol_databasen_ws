a72be258e5ac2a55a2a5cdcf22e819bf7497f6ad
Gustav Sand <sandgstv@gmail.com>
2025-07-16 01:00:08 +0200

    Lite närmre något som fungerar

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from collections import defaultdict
 import os
 
 app = Flask(__name__, instance_relative_config=True)
@@ -17,28 +18,41 @@ class Beer(db.Model):
     name = db.Column(db.String(100), nullable=False)
     brewery = db.Column(db.String(100), nullable=False)
     competition = db.Column(db.String(100), nullable=False)
+    sign = db.Column(db.String(100), nullable=True)
+    # Hur gör man om man vill lägga till kolumn?
 
-# Skapa databasen
+# Databasen skapas
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def index():
     beers = Beer.query.all()
-    return render_template('index.html', beers=beers)
+
+    comp_grouped_beers = defaultdict(list)
+    sign_grouped_beers = defaultdict(list) 
+    # Listor kommer att behöva sorteras!
+    
+    for beer in beers:
+        comp_grouped_beers[beer.competition].append(beer)
+        sign_grouped_beers[beer.sign].append(beer)
+
+    # grouped_beers är en dict med key = sign, value = lista med beers
+    return render_template('index.html', beers=beers, comp_grouped_beers=comp_grouped_beers, sign_grouped_beers=sign_grouped_beers)
 
 @app.route('/add_beer', methods=['POST'])
 def add_beer():
     name = request.form['name'].strip()
     brewery = request.form['brewery'].strip()
     competition = request.form['competition'].strip()
+    sign = request.form['sign'].strip()
 
     # Dubblettkontroll
-    existing = Beer.query.filter_by(name=name, competition=competition).first()
+    existing = Beer.query.filter_by(name=name, brewery=brewery, competition=competition).first()
     if existing:
-        return "Denna öl är redan registrerad för tävlingen!", 400
+        return "Denna öl är redan registrerad för tävlingen!", 400 # 400 betyder att det gick fel (200 = Bra)
 
-    new_beer = Beer(name=name, brewery=brewery, competition=competition)
+    new_beer = Beer(name=name, brewery=brewery, competition=competition, sign=sign)
     db.session.add(new_beer)
     db.session.commit()
 
